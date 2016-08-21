51cc6f8923a9ee6ca4da97f16af45544bab7945f
Hardian Jatmiko <miko@labster.com>
2016-08-21 16:14:05 +0800

    Add testing for InputEx.Hold() and InputEx.Release()

--- SimpleInputEx/Assets/Script/Testing/TranslateByInputTest.cs
+++ SimpleInputEx/Assets/Script/Testing/TranslateByInputTest.cs
@@ -5,11 +5,16 @@ using UnityEngine.Networking.Match;
 public class TranslateByInputTest : MonoBehaviour
 {
 	public bool InputReplay = false;
+	public Transform DestinationTransform;
+
 	public Collider Collider;
 
+	private Vector3 _initialPosition;
+
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
+		_initialPosition = transform.position;
 		InputEx.Replay(InputReplay);
 	}
 	
@@ -36,6 +41,15 @@ public class TranslateByInputTest : MonoBehaviour
 				if (Collider.Raycast(ray, out hit, 1000))
 				{
 					Debug.Log("MouseClicked in Sphere");
+					InputEx.Hold();
+					if (transform.position == _initialPosition)
+					{
+						iTween.MoveTo(gameObject, iTween.Hash("position", DestinationTransform.position, "time", 2.0f, "oncomplete", "OnMoveComplete"));
+					}
+					else
+					{
+						iTween.MoveTo(gameObject, iTween.Hash("position", _initialPosition, "time", 2.0f, "oncomplete", "OnMoveComplete"));
+					}
 				}
 			}
 		}
@@ -51,5 +65,10 @@ public class TranslateByInputTest : MonoBehaviour
 		Debug.Log("OnDestroy");
 
 		InputEx.Save();
+	}
+
+	private void OnMoveComplete()
+	{
+		InputEx.Release();
 	}
 }
